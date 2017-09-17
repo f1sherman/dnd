d2b0864db3033f32563a930b7f10a327c66fb045
Brian John <brian@brianjohn.com>
2017-09-17 09:07:30 -0500

    Initial POC

--- lib/dnd.rb
+++ lib/dnd.rb
@@ -1,5 +1,35 @@
 require "dnd/version"
+require "optparse"
+require "chronic_duration"
 
 module Dnd
-  # Your code goes here...
+  def self.run
+    options = {}
+
+    OptionParser.new do |opts|
+      opts.on "-d DURATION", "--duration DURATION", "Duration to enable Do Not Disturb" do |duration|
+        options[:duration] = ChronicDuration.parse duration
+      end
+    end.parse!
+
+    @end = Time.now + options[:duration]
+    puts "Enabling Do Not Disturb until #{@end}"
+
+    system "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true"
+    system "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date '#{Time.now}'"
+    system "killall NotificationCenter"
+
+    daemonize
+  end
+
+  def self.daemonize
+    Process.daemon
+
+    while Time.now < @end
+      sleep 60
+    end
+
+    system "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false"
+    system "killall NotificationCenter"
+  end
 end
